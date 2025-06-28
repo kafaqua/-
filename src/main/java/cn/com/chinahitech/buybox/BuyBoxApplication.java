54e93ae2e1fb290a7eb77b494cec6a154171999b
kafaqua <1597135441@qq.com>
2025-06-28 16:24:49 +0800

    上传后端模板

---
+++ src/main/java/cn/com/chinahitech/buybox/BuyBoxApplication.java
@@ -0,0 +1,13 @@
+package cn.com.chinahitech.buybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/chinahitech/buybox/BuyBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.chinahitech.buybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
